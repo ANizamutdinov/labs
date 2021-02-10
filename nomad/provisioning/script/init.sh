a991d2fd2e70be8adf75f95dcc37e3d57c04f733
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-02-10 17:00:08 +0500

    Pre-define grub-pc settings to avoid dpkg-errors on apt-upgrade

--- nomad/provisioning/script/init.sh
+++ nomad/provisioning/script/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export DEBIAN_FRONTEND=noninteractive
+echo "set grub-pc/install_devices /dev/sda" | debconf-communicate
 wget -qO - https://apt.releases.hashicorp.com/gpg | apt-key add -
 echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashi.list
 apt-get -qq update
